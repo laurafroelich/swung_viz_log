b00271d70590bd11dde9ddc92e7bbe9f647e0dd1
Laura Froelich <laura.froelich@thinkbiganalytics.com>
2018-06-09 16:22:08 +0200

    Added function to count number of files in directory, disregarding file type, including subfolders

--- code/calculate_qc_stats.py
+++ code/calculate_qc_stats.py
@@ -1,7 +1,7 @@
 import lasio
 import os
 
-def count_number_of_files(directory, extension):
+def count_number_of_particular_files(directory, extension):
     """
     Count number of files in directory (including all subdirectories) of file type extension.
 
@@ -17,3 +17,19 @@ def count_number_of_files(directory, extension):
                 count = count+1
 
     return count
+
+def count_number_of_files(directory):
+    """
+    Count number of files in directory (including all subdirectories) of file type extension.
+
+    :param directory: path to folder in which files of certain type need to be counted.
+    :return: number of files in directory.
+    """
+
+    count = 0
+    for dirpath, dirnames, filenames in os.walk(directory):
+        for filename in filenames:
+                count = count+1
+
+    return count
+
--- tests/test_qc_stats.py
+++ tests/test_qc_stats.py
@@ -3,6 +3,10 @@ import calculate_qc_stats
 
 class TestQCStats(unittest.TestCase):
 
-    def test_count_folders(self):
-        n_files = calculate_qc_stats.count_number_of_files('test_data/', 'txt')
+    def test_count_files_of_type(self):
+        n_files = calculate_qc_stats.count_number_of_particular_files('test_data/', 'txt')
         self.assertEquals(n_files, 5)
+
+    def test_count_files(self):
+        n_files = calculate_qc_stats.count_number_of_files('test_data/')
+        self.assertEquals(n_files, 6)
