a575187ed2e8c6981bcd7dbc1f9e7abc531e7d93
Laura Froelich <laura.froelich@thinkbiganalytics.com>
2018-06-09 11:55:46 +0200

    Adding initial skeleton for interactive plots page

--- code/app.py
+++ code/app.py
@@ -1,15 +1,22 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+# Landing page
+@app.route('/index.html')
+@app.route('/index')
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
 
 
 @app.route('/bootstrap')
 def bootstrap():
-    #return 'Hello again'
     return render_template('minimal_bootstrap.html')
+
+
+@app.route('/interactive_plots')
+def bootstrap():
+    return render_template('interactive_plots.html')
 
 if __name__ == '__main__':
     app.run()
