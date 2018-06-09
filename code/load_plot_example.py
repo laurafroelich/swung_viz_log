ff1809deb0c266fb9ca62818773333ff3f8f6812
Laura Froelich <laura.froelich@thinkbiganalytics.com>
2018-06-09 09:45:32 +0200

    code that should run, but does not actually plot anything yet

---
+++ code/load_plot_example.py
@@ -0,0 +1,9 @@
+import lasio
+import matplotlib.pyplot as plt
+
+def run_example(file_name):
+    print("Hello World")
+
+if __name__ == "__main__":
+    example_file = ""
+    run_example(example_file)
