5d565f5d1b81acc87f00fed6680c9d2f32dee5fc
Laura Froelich <laura.froelich@thinkbiganalytics.com>
2018-06-09 11:55:05 +0200

    Wrapping lasio reader into a read function

---
+++ tests/test_read_data.py
@@ -0,0 +1,10 @@
+import unittest
+import read_data
+import lasio
+
+class Reading(unittest.TestCase):
+
+    def test_reader(self):
+        input_file = "../../EAGE2018/Well-A_finished/HQLD_B_2C1_75-1_Well-A_ISF-BHC-MSFL-GR__COMPOSIT__1.LAS"
+        result = read_data.read(input_file)
+        assert(isinstance(result, lasio.las.LASFile))
