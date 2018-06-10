038eb73c06820da7cf12e8147e14762c3d312269
Laura Froelich <laura.froelich@thinkbiganalytics.com>
2018-06-10 10:54:47 +0200

    Removed keyword sort=True from append because of error: TypeError: append() got an unexpected keyword argument 'sort' in line 29 of holoMagic.py

--- code/holoMagic.py
+++ code/holoMagic.py
@@ -26,7 +26,7 @@ for i, item in enumerate(j_data):
     if i == 0:
         p_data = pd.DataFrame(item)
     else:
-        p_data = p_data.append(pd.DataFrame(item), sort=True)        
+        p_data = p_data.append(pd.DataFrame(item))
 
 df1 = (p_data[p_data['File_Name'] == 'GR_RES_Well-X-27.las']).copy()
 df2 = (p_data[p_data['File_Name'] == 'GR_RES_Well-I_A.LAS']).copy()
