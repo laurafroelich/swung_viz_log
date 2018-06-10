4d95cd59ef423ee78aff1befdde7bb94dc6b8822
Christian Hirsch <christian@math.aau.dk>
2018-06-10 09:01:32 +0200

    Holomagic

--- code/holoMagic.py
+++ code/holoMagic.py
@@ -1,6 +1,7 @@
 from os.path import join, dirname
 
 import pandas as pd
+import numpy as np
 import lasio
 
 from bokeh.io import curdoc
@@ -9,16 +10,13 @@ from bokeh.models import ColumnDataSource, Select
 from bokeh.palettes import Blues4
 from bokeh.plotting import figure
 
-DATA_PATH = "../../data/EAGE2018/"
+DATA_PATH = "../data/EAGE2018/"
 
-MEAS_SIZE = .15
-INDEX_WINDOW = 100
-DEPTH_WINDOW = INDEX_WINDOW * MEAS_SIZE
-
+#file names
 file1 = "{}Well-A_finished/HQLD_B_2C1_75-1_Well-A_ISF-BHC-MSFL-GR__COMPOSIT__1.LAS".format(DATA_PATH)
 file2 = "{}Well-AA_finished/HQLD_B_2C1_85-1_BHC-GR_COMPOSITED_1.LAS".format(DATA_PATH)
 
-def get_dataset(src):
+def get_dataset(src, window_size=100):
     """Prepare dataset for plotting
 
     Compute rolling averages of a src dataframe and convert to bokeh format
@@ -30,19 +28,30 @@ def get_dataset(src):
     df=src.to_frame().copy()
     df.columns = ['val']
 
-    df['left'] = df.index - DEPTH_WINDOW
-    df['roll_max'] = df['val'].rolling(INDEX_WINDOW).max()
-    df['roll_min'] = df['val'].rolling(INDEX_WINDOW).min()
+    depth_step = np.mean((df.index.values[:-1] - df.index.values[1:]))
+    depth_window_size = window_size * depth_step
+
+    df['left'] = df.index - depth_window_size
+    df['roll_max'] = df['val'].rolling(window_size).max()
+    df['roll_min'] = df['val'].rolling(window_size).min()
     return ColumnDataSource(data=df)
 
-def make_plot(source1, source2, title):
-    plot = figure( plot_width=800, plot_height=800, tools="", toolbar_location=None)#x_axis_type="datetime",
-    #plot = df1['GR'].plot().get_figure()
-    #plot.title.text = title
+def make_plot(current, average, curve, plot_width=800, plot_height=1000):
+    """Show plot of current data vs. average
 
-    #plot.line(y='DEPT', x='val', source = source2, color=Blues4[2],  legend="W2")
-    plot.quad(top='left', bottom='DEPT', left='roll_min', right='roll_max', source = source2, color=Blues4[2],  legend="W2")
-    plot.line(y='DEPT', x='val', source = source1, color=Blues4[1],  legend="W1")
+    Compute plot of current data vs. averaged data for outlier detection
+    # Arguments
+        current: bokeh Source object containing the current data
+        average: bokeh Source object containing the averaged data
+        curve: curve to be plotted
+    # Returns
+        A Source object containing the rolling averages
+    """
+
+    plot = figure( plot_width=plot_width, plot_height=plot_height, tools="", toolbar_location=None)
+    
+    plot.quad(top='left', bottom='DEPT', left='roll_min', right='roll_max', source=average, color=Blues4[2],  legend="Average")
+    plot.line(y='DEPT', x='val', source=current, color=Blues4[1],  legend="Current data")
             
             
 
@@ -55,8 +64,8 @@ def make_plot(source1, source2, title):
     """
 
     # fixed attributes
-    plot.xaxis.axis_label = None
-    plot.yaxis.axis_label = "Temperature (F)"
+    plot.xaxis.axis_label = curve
+    plot.yaxis.axis_label = "Depth"
     plot.axis.axis_label_text_font_style = "bold"
     #plot.x_range = DataRange1d(range_padding=0.0)
     plot.grid.grid_line_alpha = 0.3
@@ -64,12 +73,13 @@ def make_plot(source1, source2, title):
     return plot
 
 def update_plot(attrname, old, new):
-    city = city_select.value
-    plot.title.text = "Weather data for " + city
+    curve = curve_select.value
+    plot.title.text = curve
+    plot.xaxis.axis_label = curve
 
     print('reloading ds')
     
-    src1, src2 = [get_dataset(df[city]) for df in [df1,df2]]
+    src1, src2 = [get_dataset(df[curve]) for df in [df1,df2]]
     print('ds updated')
 
     source1.data.update(src1.data)
@@ -78,36 +88,21 @@ def update_plot(attrname, old, new):
 df1,df2 = [lasio.read(fname).df() for fname in [file1, file2]]
 df1.index = -df1.index
 df2.index = -df2.index
+print()
 
-city = 'Austin'
-
-cities = {
-    'Austin': {
-        'airport': 'AUS',
-        'title': 'Austin, TX',
-    },
-    'Boston': {
-        'airport': 'BOS',
-        'title': 'Boston, MA',
-    },
-    'Seattle': {
-        'airport': 'SEA',
-        'title': 'Seattle, WA',
-    }
-}
-
-city_select = Select(value=city, title='City', options=list(df2.columns.values))
+curve = 'GR'
+curve_select = Select(value=curve, title='Curve', options=list(df2.columns.values))
 
 
 #df = pd.read_csv(join(dirname(__file__), 'data/2015_weather.csv'))
 source1, source2 = [get_dataset(df['GR']) for df in [df1,df2]]
 print('dataset DONE')
-plot = make_plot(source1, source2, "Weather data for " + cities[city]['title'])
+plot = make_plot(source1, source2, curve)
 
-city_select.on_change('value', update_plot)
+curve_select.on_change('value', update_plot)
 #distribution_select.on_change('value', update_plot)
 
-controls = column(city_select)#, distribution_select)
+controls = column(curve_select)#, distribution_select)
 
 curdoc().add_root(row(plot, controls))
 curdoc().title = "Weather"
